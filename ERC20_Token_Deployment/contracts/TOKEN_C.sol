ef8941caf258c21e1e03e04087364ca9cf18982c
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-02-26 15:06:07 +0500

    update_contracts

---
+++ ERC20_Token_Deployment/contracts/TOKEN_C.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TOKEN_A is ERC20, Ownable {
+    constructor(uint256 _initial_supply) ERC20("TOKEN_3", "T3") {
+        _mint(msg.sender, _initial_supply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
