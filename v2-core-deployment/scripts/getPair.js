0cd40dc3a47bea769bb05d3104f14adc85c16c22
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-02-27 19:38:16 +0500

    update_getPair.js_&_run_it

--- v2-core-deployment/scripts/getPair.js
+++ v2-core-deployment/scripts/getPair.js
@@ -8,7 +8,11 @@ const provider = new ethers.providers.JsonRpcProvider(API_URL_INFURA);
 // set up contract interface
 const uniswapV2FactoryABI = require("../artifacts/contracts/UniswapV2Factory.sol/UniswapV2Factory.json");
 
-const uniswapV2FactoryContract = new ethers.Contract(uniswapV2FactoryAddress, uniswapV2FactoryABI, provider);
+const uniswapV2FactoryContract = new ethers.Contract(uniswapV2FactoryAddress, uniswapV2FactoryABI.abi, provider);
 
-const pairAddress = await uniswapV2FactoryContract.getPair(tokenAAddress, tokenBAddress);
-console.log("Pair address:", pairAddress);
+async function getPair() {
+    const pairAddress = await uniswapV2FactoryContract.getPair(tokenAAddress, tokenBAddress);
+    console.log("Pair address:", pairAddress);
+    // Pair address: 0xB071F63d994B13f7Cf29DC985Dd398A907cd975F
+};
+getPair();
