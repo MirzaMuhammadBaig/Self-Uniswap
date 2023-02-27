31c6b71a7dc45307c6e9b2cd9577f7f38852e17a
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-02-27 19:40:33 +0500

    update_allPairs.js_&_run_it

--- v2-core-deployment/scripts/allPairs.js
+++ v2-core-deployment/scripts/allPairs.js
@@ -7,16 +7,19 @@ const provider = new ethers.providers.JsonRpcProvider(API_URL_INFURA);
 
 // set up contract interface
 const uniswapV2FactoryABI = require("../artifacts/contracts/UniswapV2Factory.sol/UniswapV2Factory.json");
-const uniswapV2FactoryContract = new ethers.Contract(uniswapV2FactoryAddress, uniswapV2FactoryABI, provider);
+const uniswapV2FactoryContract = new ethers.Contract(uniswapV2FactoryAddress, uniswapV2FactoryABI.abi, provider);
 
-// call allPairsLength function to get the total number of pairs
-const allPairsLength = await uniswapV2FactoryContract.allPairsLength();
-
-// loop through all pairs and get their addresses
-const pairs = [];
-for (let i = 0; i < allPairsLength; i++) {
-  const pairAddress = await uniswapV2FactoryContract.allPairs(i);
-  pairs.push(pairAddress);
-}
-
-console.log("Pairs:", pairs);
+async function allPairs() {
+  // call allPairsLength function to get the total number of pairs
+  const allPairsLength = await uniswapV2FactoryContract.allPairsLength();
+  
+  // loop through all pairs and get their addresses
+  const pairs = [];
+  for (let i = 0; i < allPairsLength; i++) {
+    const pairAddress = await uniswapV2FactoryContract.allPairs(i);
+    pairs.push(pairAddress);
+  }
+  console.log("Pairs:", pairs);
+  //Pairs: [ '0xB071F63d994B13f7Cf29DC985Dd398A907cd975F' ]
+};
+allPairs();
