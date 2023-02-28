d7080d49bfd6828711e1e5c06b33bae13d7edf05
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2023-02-28 08:49:56 +0500

    update_this_&_run_it

--- v2-periphery-deployment/scripts/WETH.js
+++ v2-periphery-deployment/scripts/WETH.js
@@ -6,11 +6,12 @@ const provider = new ethers.providers.JsonRpcProvider(API_URL_INFURA);
 
 const abi = require("../artifacts/contracts/UniswapV2Router02.sol/UniswapV2Router02.json");
 
-const contract = new ethers.Contract(routerAddress, abi, provider);
+const contract = new ethers.Contract(routerAddress, abi.abi, provider);
 
 async function getWETH() {
   const wethAddress = await contract.WETH();
   console.log("WETH address:", wethAddress);
+  // WETH address: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
 }
 
 getWETH();
--- v2-periphery-deployment/scripts/setRevertMsg.js
+++ v2-periphery-deployment/scripts/setRevertMsg.js
@@ -2,19 +2,20 @@ const { ethers } = require("ethers");
 require("dotenv").config();
 const { API_URL_INFURA, PRIVATE_KEY, routerAddress} = process.env;
 
+const abi = require("../artifacts/contracts/UniswapV2Router02.sol/UniswapV2Router02.json");
+
 const provider = new ethers.providers.JsonRpcProvider(API_URL_INFURA);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-const abi = require("../artifacts/contracts/UniswapV2Router02.sol/UniswapV2Router02.json");
 
 const contract = new ethers.Contract(routerAddress, abi.abi, wallet);
 
-const newRevertMsg = "<insert new custom revert message here>";
+const newRevertMsg = "New revert message";
 
 async function setRevertMsg() {
   const tx = await contract.setRevertMsg(newRevertMsg);
-  console.log("Transaction hash:", tx.hash);
   const receipt = await tx.wait();
+  console.log("Transaction hash:", tx.hash);
   console.log("Transaction confirmed in block:", receipt.blockNumber);
 }
 
