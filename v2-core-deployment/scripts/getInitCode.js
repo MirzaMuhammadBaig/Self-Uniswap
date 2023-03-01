237bbab85ec7160079c0dd1def875c8db594be6f
MirzaMuhammadBaig <mbaig4893@gmail.com>
2023-03-01 18:28:56 +0500

    deployed_another_contract_run_a_script

--- v2-core-deployment/scripts/getInitCode.js
+++ v2-core-deployment/scripts/getInitCode.js
@@ -6,13 +6,13 @@ const { API_URL_INFURA, uniswapV2FactoryAddress } = process.env;
 const provider = new ethers.providers.JsonRpcProvider(API_URL_INFURA);
 
 // set up contract interface
-const uniswapV2FactoryABI = require("../artifacts/contracts/UniswapV2ERC20.sol/UniswapV2ERC20.json");
+const uniswapV2FactoryABI = require("../artifacts/contracts/UniswapV2Factory.sol/UniswapV2Factory.json");
 
 const uniswapV2FactoryContract = new ethers.Contract(uniswapV2FactoryAddress, uniswapV2FactoryABI.abi, provider);
 
 async function getInitCode() {
     const init_code_hash = await uniswapV2FactoryContract.INIT_CODE_HASH();
     console.log("INIT CODE HASH:", init_code_hash);
-    // Pair address: 0x86B56B345b8F1bfbB5f422652506102877e80B20;
+    // INIT CODE HASH: 0x7beb88f9bbd0ed45ee5366b951e02100d51484639e43d50441ad9504f0e67d9d
 }
 getInitCode();
--- v2-periphery-deployment/scripts/deploy.js
+++ v2-periphery-deployment/scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
   const UniswapV2Router02 = await hre.ethers.getContractFactory("UniswapV2Router02");
   const uniswapV2Router02 = await UniswapV2Router02.deploy(factory_address, WETH_address);
 
-  await uniswapV2Router02.deployed();
+  // await uniswapV2Router02.deployed();
 
   console.log(
     `Contract address of UniswapV2Router02: ${uniswapV2Router02.address}`
