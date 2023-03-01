313d10c872c055e59e2924dd43e786a3c4266b17
MirzaMuhammadBaig <mbaig4893@gmail.com>
2023-03-01 16:02:39 +0500

    trying_to_deployment

--- v2-core-deployment/hardhat.config.js
+++ v2-core-deployment/hardhat.config.js
@@ -40,7 +40,7 @@ module.exports = {
       url: API_URL_INFURA,
       accounts: [`0x${PRIVATE_KEY}`],
       gasMultiplier: 1,
-      gas: 21000000,
+      gas: 210000000,
       gasPrice: 80000000000,
       timeout: 20000,
       saveDeployments: true,
