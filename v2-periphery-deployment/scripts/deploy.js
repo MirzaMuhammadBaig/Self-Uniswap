53f6ea4f983726b635aa0832879a90a724efa977
MirzaMuhammadBaig <mbaig4893@gmail.com>
2023-03-01 18:30:47 +0500

    contract_deployed

--- v2-periphery-deployment/scripts/deploy.js
+++ v2-periphery-deployment/scripts/deploy.js
@@ -1,9 +1,10 @@
 const hre = require("hardhat");
-const { factory_address, WETH_address} = process.env;
+require("dotenv").config();
+const { factoryAddress, WETH_address } = process.env;
 
 async function main() {
   const UniswapV2Router02 = await hre.ethers.getContractFactory("UniswapV2Router02");
-  const uniswapV2Router02 = await UniswapV2Router02.deploy(factory_address, WETH_address);
+  const uniswapV2Router02 = await UniswapV2Router02.deploy(factoryAddress, WETH_address);
 
   // await uniswapV2Router02.deployed();
 
